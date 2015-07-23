0b4e978a258c02eeab470a4ff584f603b3931a25
dobomiraboycheva <dobromira.boycheva@gmail.com>
2015-07-23 14:33:09 +0300

    Unit Testing Task 1 Student and courses

---
+++ Unit Testing/Unit testing/Student and courses.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Student and courses.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Student and courses.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e81c923-8470-49c2-a880-9e6800607f3f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Unit Testing/Unit testing/Students and courses/Validator.cs
@@ -0,0 +1,27 @@
+﻿namespace Students_and_courses
+{
+    using System;
+
+    public static class Validator
+    {
+        public static bool IsValidName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name);
+        }
+
+        public static bool IsCoureValueNull(Course value)
+        {
+            return value == null;
+        }
+
+        public static bool IsStudentValueNull(Student value)
+        {
+            return value == null;
+        }
+
+        public static bool IsValidUniqueNumber(int uniqueNumber, int min, int max)
+        {
+            return min <= uniqueNumber && uniqueNumber <= max;
+        }
+    }
+}
